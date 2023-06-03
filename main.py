6afb5be4bb97b8732e85574f238cf1589fbee879
tuncaysahin <135434149+Tuncay-Sahin@users.noreply.github.com>
2023-06-04 02:17:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+import turtle
+
+drawing_board = turtle.getscreen()
+drawing_board.bgcolor("light blue")
+drawing_board.title("python Turtle")
+
+'''
+turtle_instance = turtle.Turtle()
+turtle_instance.forward(100)
+turtle_instance_2 = turtle.Turtle()
+turtle_instance_2.left(180)
+turtle_instance_2.forward(100)
+'''
+#square
+'''
+turtle_instance = turtle.Turtle()
+turtle_instance.forward(200)
+turtle_instance.left(90)
+turtle_instance.forward(200)
+turtle_instance.left(90)
+turtle_instance.forward(200)
+turtle_instance.left(90)
+turtle_instance.forward(200)
+'''
+#loop
+'''
+for i in range(4):
+    turtle_instance.forward(200)
+    turtle_instance.left(90)
+
+#star
+turtle_instance = turtle.Turtle()
+for i in range(5):
+    turtle_instance.left(144)
+    turtle_instance.forward(100)
+'''
+#polygon
+turtle_instance = turtle.Turtle()
+num_sides = 6
+angle = 360.0 / num_sides
+side_length = 100
+
+for i in range(num_sides):
+    turtle_instance.right(angle)
+    turtle_instance.forward(side_length)
+
+turtle.done()
